dbf4d1809d543f53a9da196ab7fc6ceb5c74af4b
Luke Mondy <luke.s.mondy@gmail.com>
2018-04-12 19:41:51 +1000

    Split classify regime into two functions

---
+++ scripts/paraview_macros/classify_regime_2D.py
@@ -0,0 +1,60 @@
+try:
+    paraview.simple
+except:
+    from paraview.simple import *
+
+XDMF_temporalFields_xmf = GetActiveSource()
+
+ProgrammableFilter1 = ProgrammableFilter()
+
+ProgrammableFilter1.RequestUpdateExtentScript = ''
+ProgrammableFilter1.PythonPath = ''
+ProgrammableFilter1.RequestInformationScript = ''
+
+ProgrammableFilter1.Script = """
+import numpy as np
+
+data = inputs[0]
+
+sigma1 = data.PointData["EigVec_1"]
+sigma3 = data.PointData["EigVec_2"]
+
+dirs = []
+mags = []
+
+a = np.array([0, 1])
+
+for s1, s3 in zip(sigma1, sigma3):
+    # Get the angle from horizontal
+    s1p = np.degrees(np.arccos(np.dot(np.absolute(s1), a)))
+    s3p = np.degrees(np.arccos(np.dot(np.absolute(s3), a)))
+   
+    # Get the plunge
+    s1p = 90 - s1p
+    s3p = 90 - s3p
+    
+    regime = np.NAN
+    mag = np.NAN
+    
+    if 60 < s1p <= 90:
+        regime = 0 # "E"
+        mag = 1 - ((90 - s1p)/30)
+    if 60 < s3p <= 90:
+        regime = 2 # "C"
+        mag = 1 - ((90 - s3p)/30)
+    dirs.append(regime)
+    try:
+        mags.append(mag)
+    except:
+        mags.append(np.NAN)
+
+npdirs = np.array(dirs)
+npmags = np.array(mags)
+output.PointData.append(npdirs, "SigDir")
+output.PointData.append(npmags, "Mags")
+"""
+SetActiveSource(ProgrammableFilter1)
+
+DataRepresentation = Show()
+
+Render()
